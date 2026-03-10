606e3e2425db45166c2e512a9d78be32b9a0cae4
Prathana Kalai <kalaiprathana21@gmail.com>
2026-03-10 12:00:52 +0530

    Initial React API fetch project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Users from "./components/Users";
+
+function App() {
+  return (
+    <div>
+      <h1>Fetch Users from API</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <b>Name:</b> {user.name} <br />
+            <b>Email:</b> {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
